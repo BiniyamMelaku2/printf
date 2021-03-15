805a7f85cdeb827d9ee572852756e6b05b2271fc
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-15 17:50:07 +0000

    project implement printf function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-
+nclude "holberton.h"
 /**
  * _printf - produces output according to a format.
  * @format: format string
@@ -9,19 +8,16 @@ int _printf(const char *format, ...)
 {
 va_list arglist;
 int index = 0, j = 0, count_char = 0;
-char *msg = "Unknown:[";
-format_chars format_char [] =
-{
+format_chars format_char [] = {
 {"c", print_char},
 {"s", print_string},
-{"d", print_digit}, 
-{"i", print_digit}, 
+{"d", print_digit},
+{"i", print_digit},
 {"%", print_sign},
 {NULL, NULL},
 };
-
 va_start(arglist, format);
-while(format != NULL && format[index] != '\0')
+while (format != NULL && format[index] != '\0')
 {
 if (format[index] == '%')      /* ("%d", 23), ("%r")  */
 {
@@ -34,28 +30,10 @@ count_char += format_char[j].func(arglist);
 break;
 }
 }
-switch (format[index])
+if (j > 4)
 {
-case ('b'):
-count_char += print_converted(convert(va_arg(arglist, int), 2));
-break;
-case ('o'):
-count_char += print_converted(convert(va_arg(arglist, int), 8));
-break;
-case ('X'):
-count_char += print_converted(convert(va_arg(arglist, int), 16));
-break;
-
-default:  
-write(1, msg, 10);
-_putchar(37); 
-_putchar(format[index]);
-_putchar(']');
-count_char += 2;
+count_char += decimal_conversion(format[index], arglist);
 }
-/*if (j > 4)
-{
-}*/
 index++;
 }
 _putchar(format[index]);
