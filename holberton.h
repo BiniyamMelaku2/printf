8c5088e02889289c4487697b0a4056a2a6346079
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-15 13:46:56 +0000

    header contains all function prototypes

---
+++ holberton.h
@@ -0,0 +1,27 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * struct format_chars - defines format types
+ * @ch: char specifier i.e. c, s, %
+ * @func: pointer to function
+ */
+ 
+typedef struct special_chars{
+char *ch;
+int (*func)(va_list);
+} format_chars;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list);
+int print_digit(va_list);
+int print_sign(va_list);
+char *convert(int num, int base);
+int print_converted(char *str);
+#endif
