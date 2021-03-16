c3c9a322457edc9f324de2a06e297e183ee7da3c
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-16 20:22:10 +0000

    project implement printf function

--- _printf.c
+++ _printf.c
@@ -1,45 +1,90 @@
 #include "holberton.h"
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * _printf - produces output according to a format.
- * @format: format string
- * Return: number of printed chars
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-va_list arglist;
-int index = 0, j = 0, count_char = 0;
-format_chars format_char [] = {
-{"c", print_char},
-{"s", print_string},
-{"d", print_digit},
-{"i", print_digit},
-{"%", print_sign},
-{NULL, NULL},
-};
-va_start(arglist, format);
-while (format != NULL && format[index] != '\0')
-{
-if (format[index] == '%')      /* ("%d", 23), ("%r")  */
-{
-index++;
-for (j = 0; format_char[j].ch != NULL; j++)
-{
-if (*format_char[j].ch == format[index])
-{
-count_char += format_char[j].func(arglist);
-break;
-}
-}
-if (j > 4)
-{
-count_char += decimal_conversion(format[index], arglist);
-}
-index++;
-}
-_putchar(format[index]);
-count_char++;
-index++;
-}
-va_end(arglist);
-return (count_char);
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
