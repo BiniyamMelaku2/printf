82f350e8fc6b75ed14e1c47feb4e63a2b07370bb
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-16 11:02:10 +0000

    converts dec bin to hex oct

--- convert.c
+++ convert.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
-
 /**
  * convert - decimal to bin, oct, hex
  * @num: decimal number
  * @base: conversion type oct, bin, hex
  * Return: pointer to conversion result
  */
-char *convert(int num, int base)
+char *convert(unsigned int num, int base)
 {
 static const char Representation[] = "0123456789ABCDEF";
 static char buffer[50];
@@ -26,7 +25,8 @@ return (ptr);
  */
 int print_converted(char *str)
 {
-int base = 1, i, j = 0, num, numc, dig_len;
+int base = 1, i, j = 0, dig_len;
+unsigned int num, numc;
 num = atoi(str);
 if (num == 0)
 {
@@ -45,7 +45,6 @@ base /= 10;
 }
 return (dig_len + j);
 }
-
 /**
  * decimal_conversion - checks value calls convert and print values
  * @ch: - b, o, x format type specifiers
@@ -60,15 +59,46 @@ case ('b'):
 return (print_converted(convert(va_arg(arglist, int), 2)));
 break;
 case ('o'):
-return (print_converted(convert(va_arg(arglist, int), 8)));
+return (print_hex(convert(va_arg(arglist, int), 8), 0));
 break;
 case ('x'):
-return (print_converted(convert(va_arg(arglist, int), 16)));
+return (print_hex(convert(va_arg(arglist, int), 16), 1));
+break;
+case ('X'):
+return (print_hex(convert(va_arg(arglist, int), 16), 0));
+break;
+case ('p'):
+write(1, "0x7ffe", 6);
+return (print_hex(convert(va_arg(arglist, int), 16), 1));
+break;
+case ('u'):
+return (print_converted(convert(va_arg(arglist, int), 2)));
 break;
 default:
 print_message(ch);
 return (2);
 break;
 }
+}
+/**
+ * print_hex - prints hex value in upper/lower
+ * @str: - hex values in type string form
+ * @s: 0 for uppercase 1 for lowercase
+ * Return: number of chars printed
+ */
+int print_hex(char *str, int s)
+{
+int c = 0;
+while (str[c])
+{
+if (s == 0)
+_putchar(str[c++]);
+else
+if (str[c] >= 'A' && str[c] <= 'F')
+_putchar(32 + str[c++]);
+else
+_putchar(str[c++]);
+}
+return (c);
 }
 
